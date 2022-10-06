73b27e51a29ea85e04e0b2e073a889d128ee3e0d
magusd <vitor.lobs@gmail.com>
2022-10-05 23:48:21 -0300

    pre-commit, backend resources

---
+++ src/0-backend/ci-role.tf
@@ -0,0 +1,54 @@
+
+
+resource "aws_iam_role" "gh-deployment-role" {
+  name = "gh-deployment-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          "Federated" : aws_iam_openid_connect_provider.github-oidc.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" = "repo:magusd/flipper*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_iam_role_policy" "gh-deployment-policy-state" {
+  name = "github-deployment-policy-state"
+  role = aws_iam_role.gh-deployment-role.id
+
+  policy = templatefile("./policies/state-management.json.tftpl", {
+    bucket : aws_s3_bucket.terraform-state.bucket,
+    table : aws_dynamodb_table.terraform-lock.name
+  })
+}
+
+# resource "aws_iam_role_policy_attachment" "state-attach" {
+#   role       = aws_iam_role.gh-deployment-role.name
+#   policy_arn = aws_iam_role_policy.gh-deployment-policy-state.arn
+# }
+
+
+
+resource "aws_iam_role_policy" "gh-deployment-policy-apply" {
+  name = "github-deployment-policy-apply"
+  role = aws_iam_role.gh-deployment-role.id
+
+  policy = templatefile("./policies/apply.json.tftpl", {})
+}
+
+# resource "aws_iam_role_policy_attachment" "apply-attach" {
+#   role       = aws_iam_role.gh-deployment-role.name
+#   policy_arn = aws_iam_role_policy.gh-deployment-policy-apply.arn
+# }
---
+++ src/0-backend/state.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "190281595713-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = aws_s3_bucket.terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_acl" "terraform-state" {
+  bucket = aws_s3_bucket.terraform-state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-state" {
+  bucket = aws_s3_bucket.terraform-state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform-lock" {
+  name         = "190281595713-terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
