6a0d9d19e90884628b938bde76c2aca3d7bbeb24
magusd <vitor.lobs@gmail.com>
2022-10-06 21:24:53 -0300

    deploy react app

--- terraform/2-web-app/main.tf
+++ terraform/2-web-app/main.tf
@@ -7,7 +7,3 @@ module "blog-storage" {
 resource "aws_sns_topic" "deploy-notifications" {
   name = "react-app-deploy-notifications"
 }
-
-# npm run build
-# aws s3 sync --acl="public-read" ./build s3://app.flipper.magusd.com
-# aws sns publish --topic-arn "arn:aws:sns:us-east-2:190281595713:react-app-deploy-notifications" --message "New code deployed"
